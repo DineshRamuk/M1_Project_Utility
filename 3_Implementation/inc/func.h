2d3f6744e97407fb0b234d65a3b226324d270356
Dinesh Kumar <92357894+DineshRamuk@users.noreply.github.com>
2022-02-12 01:34:07 +0530

    Update func.h

--- 3_Implementation/inc/func.h
+++ 3_Implementation/inc/func.h
@@ -5,23 +5,6 @@
 #include <string.h>
 #include "math.h"
 #include "assert.h"
-
-#ifndef __FUNCTION_H__
-#define __FUNCTION_H__
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "math.h"
-#include "assert.h"
-
-
-
-       
-
-
-
-
-
 #endif 
 
 
